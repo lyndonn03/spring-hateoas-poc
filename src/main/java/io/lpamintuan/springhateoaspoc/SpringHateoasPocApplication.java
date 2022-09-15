48c44a3735ceed28592a3a06a38941c42baefdb4
Lyndonn Pamintuan <pamintuan.lyndonn0503@gmail.com>
2022-09-15 23:00:35 +0800

    Initial setup

---
+++ src/main/java/io/lpamintuan/springhateoaspoc/SpringHateoasPocApplication.java
@@ -0,0 +1,13 @@
+package io.lpamintuan.springhateoaspoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHateoasPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHateoasPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/lpamintuan/springhateoaspoc/SpringHateoasPocApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lpamintuan.springhateoaspoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHateoasPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
